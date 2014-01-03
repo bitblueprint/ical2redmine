46dfa0580a64bb1867a137174a38f2eabe320257
Kræn Hansen (BIT BLUEPRINT) <kh@bitblueprint.com>
2014-01-03 23:33:42 +0100

    Adding a gitignore.

--- sync.sh
+++ sync.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-DIR=$(cd "$(dirname "$0")"; pwd)
-cd $DIR
-python ical2redmine.py --settings settings.json --log debug
+python setup.py build
+
+#DIR=$(cd "$(dirname "$0")"; pwd)
+#cd $DIR
+#python ical2redmine.py --settings settings.json --log debug
